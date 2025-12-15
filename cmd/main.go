6b9e864a2f6bd2fcc9f229ef14ffa1a1f408bd06
Sokol111 <igorsokol111@gmail.com>
2025-12-15 20:34:28 +0100

    u

---
+++ cmd/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"context"
+
+	"github.com/Sokol111/ecommerce-commons/pkg/modules"
+	"go.uber.org/fx"
+	"go.uber.org/zap"
+)
+
+var AppModules = fx.Options(
+	// Infrastructure
+	modules.NewCoreModule(),
+	modules.NewPersistenceModule(),
+	modules.NewHTTPModule(),
+	modules.NewObservabilityModule(),
+	modules.NewMessagingModule(),
+	// Domain & Application
+	// mongo.Module(),
+	// application.Module(),
+	// kafka.Module(),
+
+	// HTTP
+	// http.NewHttpHandlerModule(),
+	// swaggerui.NewSwaggerModule(swaggerui.SwaggerConfig{OpenAPIContent: httpapi.OpenAPIDoc}),
+)
+
+func main() {
+	app := fx.New(
+		AppModules,
+		fx.Invoke(func(lc fx.Lifecycle, log *zap.Logger) {
+			lc.Append(fx.Hook{
+				OnStop: func(ctx context.Context) error {
+					log.Info("Application stopping...")
+					return nil
+				},
+			})
+		}),
+	)
+	app.Run()
+}
---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/Sokol111/ecommerce-attribute-service
+
+go 1.25.5
+
+require (
+	github.com/Sokol111/ecommerce-commons v0.1.50
+)
